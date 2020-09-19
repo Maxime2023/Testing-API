8f93ee0653a2e58c1b70e494e9b22054a22939a2
maxime2023 <maximedizier06500@gmail.com>
2020-09-19 18:20:21 +0200

    Updates

---
+++ precache-manifest.ca33cba859076edce1b6559c538cd43e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f690d77b247864a59fe3ddc6bafce093",
+    "url": "/Testing-API/index.html"
+  },
+  {
+    "revision": "f8c2af755eed4c7f23a5",
+    "url": "/Testing-API/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "23d53f4d30f642d3963c",
+    "url": "/Testing-API/static/css/main.b46452b0.chunk.css"
+  },
+  {
+    "revision": "f8c2af755eed4c7f23a5",
+    "url": "/Testing-API/static/js/2.89097580.chunk.js"
+  },
+  {
+    "revision": "95553147d3fe1a2cde657bc5b8b6dd2b",
+    "url": "/Testing-API/static/js/2.89097580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d53f4d30f642d3963c",
+    "url": "/Testing-API/static/js/main.1e7c13f2.chunk.js"
+  },
+  {
+    "revision": "cbf837eaa63d6488e988",
+    "url": "/Testing-API/static/js/runtime-main.13819df8.js"
+  }
+]);
