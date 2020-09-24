8e57985945fb2464468d34d18b7e170977ac62d0
Maxime2023 <51318586+Maxime2023@users.noreply.github.com>
2020-09-24 14:39:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
-import { Button, notification, Select, Checkbox, Input } from 'antd';
+import { Button, notification, Select, Checkbox, Input, Switch } from 'antd';
 
 const { Option } = Select;
 
@@ -15,9 +15,8 @@ class App extends Component {
     isHeader: true,
     value: "",
     testImage: "",
-    isLogedIn: false,
-    login: "",
-    password: ""
+    isLogedIn: true,
+    api: "transactions"
   }
 
   componentDidMount() {
@@ -35,20 +34,10 @@ class App extends Component {
 
   handleCheck = () => {
     this.setState({isChecked: !this.state.isChecked})
-  }
+  }               
+
   handleHeader = () => {
     this.setState({isHeader: !this.state.isHeader})
-  }
-
-  handleChangeLogin = (event) => {
-    this.setState({login: event.target.value});
-  }
-  handleChangePassword = (event) => {
-    this.setState({password: event.target.value});
-  }
-  handleConnect = () => {
-    if (this.state.login === "maxime.dizier.luxcarta@gmail.com" && this.state.password === "Password1!")
-      this.setState({isLogedIn: true})
   }
 
   openNotification = (str, msg, colorSent) => {
@@ -56,7 +45,6 @@ class App extends Component {
       style: {color: colorSent},
       message: msg,
       description: str,
-
     });
   }
 
@@ -69,15 +57,22 @@ class App extends Component {
     localStorage.clear();
     sessionStorage.clear();
   }
-
-  ///////////////////////mettre un login page///////////////////////
+  onChangeSwicth = (checked) => {
+    
+    if (checked === true) {
+      this.setState({api: "transactions"})
+    }
+    else {
+      this.setState({api: "map"})
+    }
+  }
 
   callApi = () => {
     const url = [
       "https://jsonplaceholder.typicode.com/todos/1",
       "https://2fthddyg5m.execute-api.eu-central-1.amazonaws.com/Prod/get/get?map=cp%2F3876_australia_vodafone_062018.map&srs=EPSG%3A3857&transparent=true&format=image%2Fpng&exceptions=application%2Fvnd.ogc.se_xml&styles=&tiled=true&feature_count=101&service=WMS&version=1.1.1&request=GetMap&layers=ORTHO&bbox=15429272.781532537%2C-4138606.4594725817%2C15439056.72115304%2C-4128822.5198520795&width=256&height=256",
       "https://3kzsy3ah3h.execute-api.eu-central-1.amazonaws.com/Prod/tms/mosaic/ndvimask/1/0/1/1/1/1.png",
-      "https://vr77y6sw57.execute-api.eu-central-1.amazonaws.com/TestCognitoToken/transactions?transactionId=5&type=PURCHASE&amount=3"
+      `https://vr77y6sw57.execute-api.eu-central-1.amazonaws.com/TestCognitoToken/${this.state.api}`
     ]
     // axios.get(url[this.state.apiSelected], this.state.config).then((response) => {
     //   var test = btoa(unescape(encodeURIComponent(response.data)));
@@ -86,7 +81,6 @@ class App extends Component {
     // }).catch((error) => {
     //   console.log(error)
     // })
-
 
     var xhr = new XMLHttpRequest();
     if (this.state.isChecked) {
@@ -98,14 +92,16 @@ class App extends Component {
     if (this.state.isHeader)
       xhr.setRequestHeader("Authorization", localStorage.getItem("Token"));
     xhr.onload = function (e) {
+
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
           this.setState({apiValidated: this.state.apiSelected})
+          this.setState({url: this.state.apiSelected})
+          console.log("reponse :", xhr.responseText)
           this.setState({url: xhr.responseText});
           this.openNotification("L'API a été correctement appelée", "", "green");
         } else {
           console.error(xhr.statusText);
-          console.log("ezrzer :", xhr)
           this.openNotification(`erreur status : ${xhr.status}`, "Une erreur s'est produite lors de l'appel", "red");
         }
       }
@@ -166,14 +162,27 @@ class App extends Component {
               <Option value="3">API PERSO AWS</Option>
             </Select>
           </div>
+
           <div className="call-api">
-            <Button onClick={this.callApi}>APPELER L'API</Button>
+            <Button onClick={this.callApi} style={{backgroundColor: "limegreen", color: "white"}}>APPELER L'API</Button>
           </div>
+          
         </div>
-        <div className="response">
-          { this.state.apiValidated > 0 && this.state.url ? <div><img src={this.state.url} alt="#"/></div> : <div>{this.state.url}</div>}
-          <div>{this.state.url.slice(0, 4)}</div>
-        </div>
+        {this.state.apiSelected >= 3 ?
+          <div className="switch">
+            <div className="switchLeft">
+              Sans Proxy
+            </div>
+            <div className="switchMiddle">
+              <Switch defaultChecked onChange={this.onChangeSwicth} />
+            </div>
+            <div className="switchRight">
+              Avec Proxy
+            </div>
+          </div>
+          :
+          <div></div>
+        }
       </>
       )
     }
@@ -181,13 +190,13 @@ class App extends Component {
       return (
         <div>
           <div style={style}>
-            <Input onChange={this.handleChangeLogin}></Input>
+            <Input></Input>
           </div>
           <div style={style}>
-            <Input.Password onChange={this.handleChangePassword}></Input.Password>
+            <Input></Input>
           </div>
           <div style={style}>
-            <Button onClick={this.handleConnect}>Se connecter</Button>
+            <Button>Se connecter</Button>
           </div>
         </div>
       )
